d92c0813077fe084bb399f945ddee57ad79ba7c2
Oisin Brannock <oisinbrannock@Oisins-MacBook-Pro.local>
2020-07-12 22:29:16 +0100

    Changed the cleaning notebook to be my eda and cleaned it up, and deleted the duplicate eda

--- model_build.py
+++ model_build.py
@@ -6,4 +6,13 @@ Created on Sat Jul  4 14:39:14 2020
 @author: oisinbrannock
 """
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
+df = pd.read_csv('eda_data.csv')
+
+# Choose the relevant columns to analyze for model building 
+df.columns
+
+df_model = 
